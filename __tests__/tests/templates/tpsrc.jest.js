17391c9544991ddbd42e4e54e5ebfb5038f78f89
lornelas <lornelas@slack-corp.com>
2019-10-26 13:34:35 -0700

    new test for tps rc file

--- __tests__/tests/templates/tpsrc.jest.js
+++ __tests__/tests/templates/tpsrc.jest.js
@@ -1,72 +1,19 @@
 import { TESTING_TPS } from '@test/support/constants';
 import Templates from '@test/templates';
-import path from 'path';
+import Playground from '@test/support/playground';
+import { TESTING_DIR } from '@test/support/constants';
+import fs from 'fs';
 
 /**
- * Templates testing OLD
+ * Templates testing tpsrc
  */
-// describe.skip('[Templates] Config: ', () => {
-//   describe('Using basic features', () => {
-//     let tps;
-//     beforeAll(() => {
-//       tps = new Templates('testing', {
-//         noGlobalConfig: true
-//       });
-//     });
-
-//     it('should load local configurations', () => {
-//       const config = tps.config;
-//       console.log(config);
-//       expect(config).toEqual(
-//         expect.objectContaining({
-//           name: 'testing-config'
-//         })
-//       );
-//     });
-
-//     it('should allow user to add more configurations', () => {
-//       const newConfig = {
-//         name: 'testing-new-config',
-//         age: 24
-//       };
-
-//       tps.setAnswers(newConfig);
-
-//       const config = tps.config;
-
-//       expect(config).toEqual(expect.objectContaining(newConfig));
-//     });
-
-//     it("should throw error if it doesn't receive an object as a argument", () => {
-//       const badConifgs = ['hey', false, true, 3456, []];
-
-//       badConifgs.forEach(badConifg => {
-//         expect(() => {
-//           tps.setAnswers(badConifg);
-//         }).toThrow();
-//       });
-//     });
-//   });
-
-//   describe('Using Prompts features', () => {
-//     let tps;
-//     beforeEach(() => {
-//       tps = new Templates('testing-prompt-types-select', {
-//         noGlobalConfig: true
-//       });
-//     });
-
-//     it('should answer prompt when config is loaded', () => {
-//       tps.setAnswers({ cssType: 'less' });
-
-//       expect(tps._prompts.needsAnswers()).toBeFalsy();
-
-//       expect(tps._prompts.answers).toHaveProperty('cssType', 'less');
-//     });
-//   });
-// });
+const playground = new Playground(TESTING_DIR);
+const extendedDest = './new-path';
 
 describe('[Templates] tpsrc: ', () => {
+  beforeAll(() => playground.create());
+  afterAll(() => playground.destroy());
+
   let tps;
   beforeAll(() => {
     tps = new Templates('testing-tpsrc', {
@@ -79,7 +26,7 @@ describe('[Templates] tpsrc: ', () => {
     const answers = tps._prompts.answers;
 
     expect(opts).toMatchObject({
-      extendedDest: './new-path'
+      extendedDest
     });
 
     expect(answers).toEqual(
@@ -87,5 +34,19 @@ describe('[Templates] tpsrc: ', () => {
         test: 'oh-yea'
       })
     );
+  });
+
+  describe('when adding options in tpsrc', () => {
+    beforeEach(() => playground.createBox('templates_tpsrc'));
+
+    it('should be able to use extendDest', () => {
+      const dest = playground.pathTo(`${extendedDest}`);
+      const file = `${dest}/App/index.js`;
+      fs.mkdirSync(dest);
+
+      return tps.render(playground.box(), 'App').then(() => {
+        expect(file).toBeFile();
+      });
+    });
   });
 });
