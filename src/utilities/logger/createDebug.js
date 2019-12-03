94abacdb2788a576c85bff8af08f11e7a54ebaf8
lornelas <lornelas@slack-corp.com>
2019-12-03 01:43:36 -0800

    separated logger into different files

--- src/utilities/logger/createDebug.js
+++ src/utilities/logger/createDebug.js
@@ -1,108 +1,7 @@
 import debug from 'debug';
-import colors from 'ansi-colors';
-import pjson from 'prettyjson-256';
 import is from 'is';
-import { defaults } from '@tps/utilities/helpers';
+import './formatters';
 import CreateDebugGroup from './createDebugGroup';
-
-process.env.DEBUG_COLORS = false;
-CreateDebugGroup;
-
-/**
- * Constants
- */
-
-const newColors = {
-  info: colors.blue,
-  log: colors.blue,
-  error: colors.red,
-  warn: colors.yellow,
-  success: colors.green
-};
-
-const levels = Object.keys(newColors);
-
-const TITLES_RE = (function() {
-  const regStr = levels.map(str => `:${str}`).join('|');
-  return new RegExp(regStr, 'g');
-})();
-
-const pattern = [
-  '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-  '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
-].join('|');
-
-const ANSII_RE = new RegExp(pattern, 'g');
-
-const PJSON_SETTINGS = {
-  depth: 10
-  // alphabetizeKeys: true
-  // customColors: {
-  //   red: { fg: [5, 0, 0] },
-  //   atomicTorquoise: { fg: [5, 2, 0], bg: [0, 2, 4] }
-  // }
-};
-
-/**
- * Initialize
- */
-
-colors.theme(newColors);
-
-const render = (object, indent = 0, opts = {}) => {
-  const options =
-    is.object(opts) && !is.empty(opts)
-      ? defaults(opts, PJSON_SETTINGS)
-      : PJSON_SETTINGS;
-  return pjson.render(object, indent, options);
-};
-
-/**
- * Formatters
- */
-debug.formatters.n = v => {
-  return `\n${  render(v, 2)}`;
-};
-
-/* only used when you want to use inline */
-debug.formatters.o = v => {
-  if (is.object(v)) {
-    let output = render(v, 2, {
-      // depth: 1
-    })
-      .replace(/\n/g, '')
-      .replace(/\s*:\s*/g, '=');
-
-    const matched = output.match(ANSII_RE);
-
-    matched.forEach(match => {
-      const index = output.indexOf(match) + match.length;
-      output = output.slice(0, index) + output.slice(index).trim();
-    });
-
-    return output.trim();
-  } if (is.array(v)) {
-    return v;
-  }
-  return render(v);
-};
-
-debug.formatters.s = v => {
-  return colors.white(v);
-};
-
-/* All objects, arrays */
-debug.formatters.O = v => {
-  return render(v, 2);
-};
-
-debug.log = function(string, ...rest) {
-  const filteredString = string.replace(TITLES_RE, matched => {
-    const titleName = matched.slice(1);
-    return `\u001b[0m ${  colors[titleName](titleName)}`;
-  });
-  console.log(filteredString, ...rest);
-};
 
 const logFunctions = ['info', 'error', 'debug', 'success', 'warn'];
 
@@ -118,6 +17,7 @@ class CreateDebug {
       debug.enable(enableLogFuntions);
     }
 
+    /* I think this should be name????? */
     logFunctions.reduce((acc, logType) => {
       const logger = this.logger.extend(logType);
 
