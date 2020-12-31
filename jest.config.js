6eaa90c6f3431719bae8a1b3bfd03a17c8f4b060
lornelas <lornelas@slack-corp.com>
2020-12-30 22:09:57 -0800

    testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   moduleFileExtensions: ['ts', 'js'],
   transform: {
@@ -6,6 +8,9 @@ module.exports = {
   testMatch: ['**/__tests__/tests/**/*jest.(js|ts)'],
   setupFilesAfterEnv: ['./__tests__/setup.ts'],
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@tps/(.+)$': path.join(__dirname, 'src/$1'),
+  },
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.json',
