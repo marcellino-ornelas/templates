adefb04882b2dda2e454bcba66e94183237f4b74
lornelas <lornelas@slack-corp.com>
2021-04-21 16:04:09 -0700

    use adapter in templates

--- __tests__/templates.js
+++ __tests__/templates.js
@@ -1,7 +1,7 @@
 import Templates from '@tps/templates';
 import path from 'path';
 import { TPS_FOLDER } from '@tps/utilities/constants';
-import MemoryFileSystem from 'memory-fs';
+import MemoryFileSystem from 'memory-fs-extra';
 
 export default class TemplateOverride extends Templates {
   constructor(use, opts) {
