85911063b77f6c2d6d0d7a8edeb42e60cc39f87a
lornelas <lornelas@slack-corp.com>
2021-04-21 16:30:27 -0700

    test

--- src/fileSystemTree/directoryNode.js
+++ src/fileSystemTree/directoryNode.js
@@ -33,7 +33,7 @@ export class DirectoryNode extends FileSystemNode {
     let dirContents;
 
     try {
-      dirContents = this.fs.readdirSync(this.path);
+      dirContents = super.readdirSync(this.path);
     } catch (e) {
       throw new Error(`[TPS ERROR] Path is not a directory (${this.path})`);
     }
