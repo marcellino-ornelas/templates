57922e8e775c5eeb9c5dbd7a662e6db5c000a5e4
lornelas <lornelas@slack-corp.com>
2021-04-21 17:54:49 -0700

    test

--- __tests__/utilities/fs.js
+++ __tests__/utilities/fs.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { TESTING_TPS } from '@test/utilities/constants';
 import { DirectoryNode } from '@tps/fileSystemTree';
 
-const MemoryFileSystem = require('memory-fs-extra');
+// const MemoryFileSystem = require('memory-fs-extra');
 const fs = require('fs-extra');
 
 const fakefs = new MemoryFileSystem({});
