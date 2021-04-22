4170eba5a756108c028b13d382ddf4a5d3c59b46
lornelas <lornelas@slack-corp.com>
2021-04-21 17:53:24 -0700

    test

--- __tests__/utilities/fs.js
+++ __tests__/utilities/fs.js
@@ -1,9 +1,35 @@
 import MemoryFileSystem from 'memory-fs-extra';
+import path from 'path';
+import { TESTING_TPS } from '@test/utilities/constants';
+import { DirectoryNode } from '@tps/fileSystemTree';
 
-const fs = new MemoryFileSystem({});
+const MemoryFileSystem = require('memory-fs-extra');
+const fs = require('fs-extra');
 
-fs.outputFileSync('/hey/bad.txt', 'blah');
+const fakefs = new MemoryFileSystem({});
+const PATH = `/home/runner/work/templates/templates/__tests__/.tps/`;
 
-console.log(fs);
+fakefs.ensureDir(PATH);
+
+const tpsDirectoryContents = fs.readdirSync(TESTING_TPS);
+
+tpsDirectoryContents.forEach((dirname) => {
+  const dir = new DirectoryNode(dirname, TESTING_TPS);
+
+  dir.eachChild((child) => {
+    child;
+    const path = pasth.join(PATH, child.pathFromRoot);
+    if (child.is('dir')) {
+      fakefs.ensureDirSync(path);
+    } else {
+      fakefs.outputFileSync(path, child._getFileData());
+    }
+  });
+});
+
+fakefs.outputFileSync('/hey/bad.txt', 'blah');
+fakefs.outputFileSync('/hey/god.txt', 'blah');
+
+console.log(fakefs);
 
 export default fs;
