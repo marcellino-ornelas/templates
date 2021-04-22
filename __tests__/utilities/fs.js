3a8d15721fb24bd9c0156fe00dc79e6ede852f7e
lornelas <lornelas@slack-corp.com>
2021-04-21 18:28:53 -0700

    bl;ah

--- __tests__/utilities/fs.js
+++ __tests__/utilities/fs.js
@@ -14,29 +14,31 @@ fakefs.ensureDir(PATH);
 const tpsDirectoryContents = fs.readdirSync(TESTING_TPS);
 
 tpsDirectoryContents.forEach((dirname) => {
-  console.log(`${TESTING_TPS}/${dirname}`);
   if (fs.statSync(`${TESTING_TPS}/${dirname}`).isDirectory()) {
     const dir = new DirectoryNode(dirname, TESTING_TPS);
     dir.eachChild((child) => {
-      child;
       const path = realPath.join(PATH, child.pathFromRoot);
       if (child.is('dir')) {
         fakefs.ensureDirSync(path);
       } else {
-        fakefs.outputFileSync(path, fs.readFileSync(path));
+        fakefs.outputFileSync(path, fs.readFileSync(child.path));
       }
     });
   } else {
     const file = new FileNode(dirname, TESTING_TPS);
-    const path = realPath.join(PATH, file.pathFromRoot);
+    const path = realPath.join(PATH, file.name);
 
-    fakefs.outputFileSync(path, fs.readFileSync(path));
+    // console.log('create file', path, file);
+    fakefs.outputFileSync(
+      path,
+      fs.readFileSync(`${TESTING_TPS}/${dirname}`)
+    );
   }
 });
 
 fakefs.outputFileSync('/hey/bad.txt', 'blah');
 fakefs.outputFileSync('/hey/god.txt', 'blah');
 
-console.log(fakefs);
+console.dir(fakefs.data.home.runner.work.templates.templates.__tests__);
 
 export default fs;
