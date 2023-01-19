38a82c0c4cf88e0b2e043aeb87848b9014bb7576
Marcellino Ornelas <marcellino.ornelas@gmail.com>
2023-01-18 17:46:46 -0800

    remove module.exports

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@
 //   testEnvironment: 'node',
 // };
 
+const { pathsToModuleNameMapper } = require('ts-jest');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig');
 const path = require('path');
 
 module.exports = {
@@ -14,9 +18,11 @@ module.exports = {
   testMatch: ['**/__tests__/tests/**/*jest.(js|ts)'],
   setupFilesAfterEnv: ['./__tests__/setup.js'],
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@tps/(.+)$': path.join(__dirname, 'src/$1'),
-  },
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths /*, { prefix: '<rootDir>/' } */
+  ),
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.json',
