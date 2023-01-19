84d37ce5eab1ef0382de1283cb6c48996bf1da19
Marcellino Ornelas <marcellino.ornelas@gmail.com>
2023-01-18 17:36:57 -0800

    jest set up

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,25 @@
+// module.exports = {
+//   testMatch: ['**/__tests__/tests/**/*jest.js'],
+//   setupFilesAfterEnv: ['./__tests__/setup'],
+//   testEnvironment: 'node',
+// };
+
+const path = require('path');
+
 module.exports = {
-  testMatch: ['**/__tests__/tests/**/*jest.js'],
-  setupFilesAfterEnv: ['./__tests__/setup'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.(ts|js)?$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/tests/**/*jest.(js|ts)'],
+  setupFilesAfterEnv: ['./__tests__/setup.ts'],
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@tps/(.+)$': path.join(__dirname, 'src/$1'),
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
 };
