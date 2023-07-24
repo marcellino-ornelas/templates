514a4d2f424fe7e5b8fba681128fb10377c1beb8
Marcellino Ornelas <--global>
2023-07-24 13:34:37 -0700

    clean up

--- docs/templates-plugin.js
+++ docs/templates-plugin.js
@@ -4,13 +4,13 @@ const Templates = require('templates-mo');
 module.exports = function friendsPlugin(context, options) {
   return {
     name: 'templates-plugin',
-    async loadContent() {
-      console.log('hey', Templates);
-      return {};
-    },
-    async contentLoaded({ content, actions }) {
-      const { setGlobalData, addRoute } = actions;
-      setGlobalData({ templates: ['Yangshun', 'Sebastien'] });
-    },
+    // async loadContent() {
+    //   console.log('hey', Templates);
+    //   return {};
+    // },
+    // async contentLoaded({ content, actions }) {
+    //   const { setGlobalData, addRoute } = actions;
+    //   setGlobalData({ templates: ['Yangshun', 'Sebastien'] });
+    // },
   };
 };
