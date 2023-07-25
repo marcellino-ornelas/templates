1b1d759bfc3004695cb7b7f32ca5d02387d3584c
Marcellino Ornelas <--global>
2023-07-24 22:22:45 -0700

    clean up

--- cli/index.js
+++ cli/index.js
@@ -5,14 +5,4 @@ const yargs = require('yargs');
 const GLOBAL_OPTIONS = require('./options/global.json');
 
 // eslint-disable-next-line no-unused-expressions
-// yargs.options(GLOBAL_OPTIONS).commandDir('commands').help().argv;
-yargs
-  .options(GLOBAL_OPTIONS)
-  .commandDir('commands')
-  //   .command(require('./commands/copy'))
-  //   .command(require('./commands/list'))
-  //   .command(require('./commands/init'))
-  //   .command(require('./commands/new'))
-  //   .command(require('./commands/create'))
-  //   .command(require('./commands/use'))
-  .help().argv;
+yargs.options(GLOBAL_OPTIONS).commandDir('commands').help().argv;
