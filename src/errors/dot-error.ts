d8f3e2520494cbb0f94d902b741f5bc1c04f4371
Marcellino Ornelas <--global>
2023-07-26 13:21:26 -0700

    clean up errors

--- src/errors/dot-error.ts
+++ src/errors/dot-error.ts
@@ -1,16 +1,15 @@
 import { FileNode } from '@tps/fileSystemTree';
 
 export class DotError extends Error {
+  public name = 'DotError';
+
   public fileName: string;
 
   public path: string;
 
   constructor(fileNode: FileNode, errorMessage: string) {
-    super();
-    this.name = 'DotError';
+    super(`${errorMessage} ( ${fileNode.path} )`);
     this.fileName = fileNode.name;
     this.path = fileNode.path;
-
-    this.message = `${errorMessage} ( ${fileNode.path} )`;
   }
 }
