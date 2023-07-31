8c1f8157df22220f1e4d74989abaf43adb6f40e0
Marcellino Ornelas <--global>
2023-07-31 12:42:16 -0700

    Update react component template

--- .tps/react-component/settings.js
+++ .tps/react-component/settings.js
@@ -1,7 +1,19 @@
+// @ts-check
+
+/** @type {import('../../src/types/settings').SettingsFile} */
 module.exports = {
   prompts: [
     {
+      name: 'extention',
+      aliases: ['e', 'ext'],
+      type: 'input',
+      tpsType: 'data',
+      message: 'What type of extention do you want for your component?',
+      default: 'js',
+    },
+    {
       name: 'css',
+      aliases: ['c'],
       type: 'confirm',
       tpsType: 'package',
       message: 'Would you like to include a css file?',
@@ -9,25 +21,37 @@ module.exports = {
     },
     {
       name: 'cssType',
+      aliases: ['z'],
       tpsType: 'data',
-      type: 'list',
-      message: 'What type of css file would you like?',
-      choices: ['css', 'scss', 'less'],
+      type: 'input',
+      message: 'What type of css extention would you like?',
       when: (answers) => {
-        console.log(answers);
         return !!answers.css;
       },
       default: 'css',
     },
     {
       name: 'test',
+      aliases: ['t'],
       type: 'confirm',
       tpsType: 'package',
       message: 'Would you like to include unit tests?',
-      default: true,
+      default: false,
+    },
+    {
+      name: 'testType',
+      aliases: ['y'],
+      tpsType: 'data',
+      type: 'input',
+      message: 'What type of test extention would you like?',
+      when: (answers) => {
+        return !!answers.test;
+      },
+      default: 'test.js',
     },
     {
       name: 'index',
+      aliases: ['i'],
       type: 'confirm',
       tpsType: 'package',
       message: 'Would you like to include a index file?',
