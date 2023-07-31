3e659931c32fd3ad84da25d325151a4dcf95bd19
Marcellino Ornelas <--global>
2023-07-30 23:04:54 -0700

    use momospace

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -161,9 +161,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-  stylesheets: [
-    'https://fonts.googleapis.com/css2?family=Lato&family=Ubuntu+Mono&display=swap',
-  ],
   themes: ['@docusaurus/theme-mermaid'],
 };
 
