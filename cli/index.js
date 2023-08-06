777883bd1c3919070e2395b96058f4d317f335b5
Marcellino Ornelas <--global>
2023-08-06 13:25:10 -0700

    Delete cli v1

--- cli/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-/* eslint-disable no-unused-expressions */
-
-const yargs = require('yargs');
-const GLOBAL_OPTIONS = require('./options/global.json');
-
-// eslint-disable-next-line no-unused-expressions
-yargs.options(GLOBAL_OPTIONS).commandDir('commands').help().argv;
