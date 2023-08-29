fe99858451ef7de16a011d36410118c06cfe61e5
Marcellino Ornelas <--global>
2023-08-28 21:48:15 -0700

    Revert file back because its not needed

--- src/utilities/fileSystem.ts
+++ src/utilities/fileSystem.ts
@@ -1,12 +1,10 @@
-import fs from 'fs';
+import * as fs from 'fs';
 import * as findFileUp from 'find-up';
-
-// console.log('old filesystem', (fs as any).toTree);
 
 /**
  * Check to see if the `path` is a valid directory
  */
-export const isDir = (path: string): boolean => {
+export function isDir(path: string): boolean {
   let dir;
   try {
     dir = fs.lstatSync(path);
@@ -14,12 +12,12 @@ export const isDir = (path: string): boolean => {
     return false;
   }
   return dir.isDirectory();
-};
+}
 
 /**
  * Check to see if the `path` is a valid file
  */
-export const isFile = (path: string): boolean => {
+export function isFile(path: string): boolean {
   let file;
   try {
     file = fs.lstatSync(path);
@@ -27,20 +25,20 @@ export const isFile = (path: string): boolean => {
     return false;
   }
   return file.isFile();
-};
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const json = (jsonFile: string): any => {
+export function json(jsonFile: string): any {
   try {
     const jsonContents = fs.readFileSync(jsonFile).toString();
     return JSON.parse(jsonContents);
   } catch (err) {
     return {};
   }
-};
+}
 
-export const findUp = (folder: string, cwd: string = process.cwd()) => {
+export function findUp(folder: string, cwd: string = process.cwd()) {
   return findFileUp.sync(folder, {
     cwd,
   });
-};
+}
