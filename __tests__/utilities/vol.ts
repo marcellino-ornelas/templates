45d2c385836bc89fc1853e2d495f2ff59b35c5ed
Marcellino Ornelas <--global>
2023-08-30 21:53:06 -0700

    clean up vol

--- __tests__/utilities/vol.ts
+++ __tests__/utilities/vol.ts
@@ -5,13 +5,8 @@ import templateFiles from '@test/templates.json';
 
 export const vol = new Volume();
 
-const DEFAULT_FILES = {
-  [`${process.cwd()}/readme.md`]: '',
-  ...templateFiles,
-};
-
 vol.fromJSON({
   [`${process.cwd()}/readme.md`]: '',
   [`${process.cwd()}/__tests__/readme.md`]: '',
-  ...DEFAULT_FILES,
+  ...templateFiles,
 });
