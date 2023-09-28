4e175f25ccc2e50da03422234a131bbb7919c5b2
Marcellino Ornelas <--global>
2023-09-27 22:55:16 -0700

    fork dot

--- src/dot.ts
+++ src/dot.ts
@@ -1,4 +1,6 @@
-import * as dot from 'dot';
+// import * as dot from 'dot';
+
+import dot from '@tps/templates/template-engine';
 
 dot.templateSettings.strip = false;
 dot.templateSettings.varname = 'tps';
