164193176ce84824d2c4617e3de6c6bb8082edb1
Marcellino Ornelas <--global>
2023-10-08 20:15:33 -0700

    clean up

--- __tests__/tests/templates/opts.jest.ts
+++ __tests__/tests/templates/opts.jest.ts
@@ -1,23 +1,8 @@
-import { TESTING_DIR } from '@test/utilities/constants';
 import Templates from '@test/templates';
-import Playground from '@test/utilities/playground';
-import fs from 'fs';
 
 jest.mock('fs');
 
-/**
- * Templates testing tpsrc
- */
-const playground = new Playground(TESTING_DIR);
-
 describe('[Templates] options:', () => {
-	beforeAll(() => playground.create());
-	afterAll(() => playground.destroy());
-
-	beforeEach(async () => {
-		await playground.createBox('options');
-	});
-
 	it('should prefer options from settings file vs defaults', async () => {
 		const tps = new Templates('testing-options-settings-file');
 
