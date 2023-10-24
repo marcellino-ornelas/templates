ac3c0f09be03db78600f7d8bbf2c0d35739b2cac
Marcellino Ornelas <--global>
2023-10-23 22:27:43 -0700

    finished tests

--- __tests__/tests/default-templates/yargs-cli-cmd.jest.ts
+++ __tests__/tests/default-templates/yargs-cli-cmd.jest.ts
@@ -35,10 +35,142 @@ describe('yargs-cli-cmd', () => {
 		expect(playground.pathTo('publish.js')).toBeFile();
 	});
 
-	it('should be able ', async () => {
-		await tps.render(playground.box(), 'publish');
+	// it('should be able to ', async () => {
+	// 	await tps.render(playground.box(), 'publish');
 
-		// @ts-expect-error no types for extending jest functions
-		expect(playground.pathTo('publish.js')).toBeFile();
+	// 	// @ts-expect-error no types for extending jest functions
+	// 	expect(playground.pathTo('publish.js')).toBeFile();
+	// });
+
+	describe('Prompt: type', () => {
+		it('should be able to use named export syntax', async () => {
+			tps.setAnswers({
+				type: 'namedExport',
+			});
+
+			await tps.render(playground.box(), 'publish');
+
+			// @ts-expect-error no types for extending jest functions
+			expect(playground.pathTo('publish.js')).toHaveFileContents(`\
+export const command = 'publish';
+
+export const aliases = [];
+
+export const describe = "...";
+
+export const builder = {
+    flag: {
+        alias: '',
+        describe: '...',
+        type: 'boolean',
+    },
+};
+
+export const handler = async (argv) => {
+    // code ...
+};
+`);
+		});
+
+		it('should be able to use named export syntax', async () => {
+			tps.setAnswers({
+				type: 'defaultExport',
+			});
+
+			await tps.render(playground.box(), 'publish');
+
+			// @ts-expect-error no types for extending jest functions
+			expect(playground.pathTo('publish.js')).toHaveFileContents(`\
+export default {
+    command: 'publish',
+    aliases: [],
+    describe: "...",
+    builder: {
+        flag: {
+            alias: '',
+            describe: '...',
+            type: 'boolean',
+        },
+    },
+    async handler(argv) {
+        // code ...
+    },
+};
+`);
+		});
+	});
+
+	describe('Prompt: typescript', () => {
+		it('should be able to use typescript with default export', async () => {
+			tps.setAnswers({
+				type: 'defaultExport',
+				typescript: true,
+			});
+
+			await tps.render(playground.box(), 'publish');
+
+			// @ts-expect-error no types for extending jest functions
+			expect(playground.pathTo('publish.ts')).toHaveFileContents(`\
+import { CommandModule } from 'yargs';
+
+interface PublishArgv {
+    flag: boolean;
+}
+
+export default {
+    command: 'publish',
+    aliases: [],
+    describe: "...",
+    builder: {
+        flag: {
+            alias: '',
+            describe: '...',
+            type: 'boolean',
+        },
+    },
+    async handler(argv) {
+        // code ...
+    },
+} as CommandModule<object, PublishArgv>;
+`);
+		});
+
+		it('should be able to use typescript with default export', async () => {
+			tps.setAnswers({
+				type: 'namedExport',
+				typescript: true,
+			});
+
+			await tps.render(playground.box(), 'publish');
+
+			// @ts-expect-error no types for extending jest functions
+			expect(playground.pathTo('publish.ts')).toHaveFileContents(`\
+import { CommandModule } from 'yargs';
+
+interface PublishArgv {
+    flag: boolean;
+}
+
+type PublishModule = CommandModule<object, PublishArgv>;
+
+export const command: PublishModule['command'] = 'publish';
+
+export const aliases: PublishModule['aliases'] = [];
+
+export const describe: PublishModule['describe'] = "...";
+
+export const builder: PublishModule['builder'] = {
+    flag: {
+        alias: '',
+        describe: '...',
+        type: 'boolean',
+    },
+};
+
+export const handler: PublishModule['handler'] = async (argv) => {
+    // code ...
+};
+`);
+		});
 	});
 });
