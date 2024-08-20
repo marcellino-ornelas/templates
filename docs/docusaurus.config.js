e5d49ee5ebf5a3ddee61e93fd1435325976a160c
Marcellino Ornelas <--global>
2024-08-19 23:27:33 -0700

    Upgrade docusaurus to 3.5.2

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,11 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+// const lightCodeTheme = require('prism-react-renderer');
+// const lightCodeTheme = require('prism-react-renderer/themes/github');
+// const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer');
+
 const templatesPlugin = require('./templates-plugin');
 
 /** @type {import('@docusaurus/types').Config} */
@@ -166,8 +169,8 @@ const config = {
 				copyright: `Copyright © ${new Date().getFullYear()} Marcellino Ornelas, Inc. Built with Docusaurus.`,
 			},
 			prism: {
-				theme: lightCodeTheme,
-				darkTheme: darkCodeTheme,
+				theme: themes.github,
+				darkTheme: themes.dracula,
 			},
 		}),
 	themes: ['@docusaurus/theme-mermaid'],
