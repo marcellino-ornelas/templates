ca7a19c7df163ed165d84fdfe7d98cfe04a73f27
Marcellino Ornelas <--global>
2024-08-20 17:42:36 -0700

    clean up docusarous.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-// const lightCodeTheme = require('prism-react-renderer');
-// const lightCodeTheme = require('prism-react-renderer/themes/github');
-// const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const { themes } = require('prism-react-renderer');
 
 const templatesPlugin = require('./templates-plugin');
