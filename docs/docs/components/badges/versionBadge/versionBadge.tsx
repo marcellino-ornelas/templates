1893b71e756240b05a8da9dc4461970f1b795348
marcellino ornelas <35247622+marcellino-ornelas@users.noreply.github.com>
2024-10-22 21:14:28 -0700

    Docs: Created Badge and VersionBadge (#235)
    Co-authored-by: Marcellino Ornelas <--global>

---
+++ docs/docs/components/badges/versionBadge/versionBadge.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Badge } from '@site/docs/components/badges/badge';
+
+interface VersionBadgeProps {
+	version: string;
+}
+
+export const VersionBadge = ({ version }: VersionBadgeProps) => {
+	return <Badge type="secondary">templates@{version}</Badge>;
+};
