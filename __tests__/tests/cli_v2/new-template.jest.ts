eeede1712758a167a6bbc6d3e6699d0993e7552e
Marcellino Ornelas <--global>
2024-10-28 20:38:33 -0700

    add test for cli options

--- __tests__/tests/cli_v2/new-template.jest.ts
+++ __tests__/tests/cli_v2/new-template.jest.ts
@@ -32,4 +32,16 @@ describe('Command Line: new template', () => {
 			parser.parseAsync('new template my-template'),
 		).rejects.toThrowError('TPS template is already created.');
 	});
+
+	it('should be able to use new-template options', async () => {
+		const parser = yargs().command(newCommand).fail(false);
+
+		/**
+		 * We need answer annotate because using `js` type makes annotate get prompted
+		 */
+		await parser.parseAsync('new template my-template --type js --annotate');
+
+		// @ts-expect-error not typed yet
+		expect(path.join(CWD, '.tps/my-template/settings.js')).toBeFile();
+	});
 });
