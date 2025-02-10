cc8b31d1f25a16b63b65a6d0756d2461f53f4995
Marcellino Ornelas <--global>
2025-02-09 21:17:38 -0800

    Add prettier config template

---
+++ .tps/prettier-config/settings.js
@@ -0,0 +1,40 @@
+// @ts-check
+
+/** @type {import('../../src/types/settings').SettingsFile} */
+module.exports = {
+	prompts: [
+		{
+			name: 'type',
+			description: 'Type of config file you want to use',
+			message: 'What type of config file do you want?',
+			type: 'list',
+			tpsType: 'package',
+			choices: ['json', 'js'],
+			default: 'json',
+		},
+		{
+			name: 'spacing',
+			description: 'Type of spacing you want to use',
+			message: 'What type of spacing do you want to use?',
+			type: 'list',
+			tpsType: 'data',
+			choices: ['spaces', 'tab'],
+			default: 'spaces',
+		},
+		{
+			name: 'ignore',
+			hidden: true,
+			description: 'Renders a .prettierignore file',
+			message: 'Do you want a .prettierignore file?',
+			type: 'confirm',
+			tpsType: 'package',
+			default: true,
+		},
+	],
+	// events: {
+	// 	// async onRender(tps) {},
+	// 	// async onBuildPathRender(tps, { buildPath }) {},
+	// 	// async onBuildPathRendered(tps, { buildPath }) {},
+	// 	// async onRendered(tps, { dest, buildPaths }) {}
+	// },
+};
