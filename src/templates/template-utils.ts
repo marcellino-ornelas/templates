d9bfb97f6b5baf59bd30c2d6b424bad80751b8fc
Marcellino Ornelas <--global>
2025-02-20 20:38:26 -0800

    clean up

--- src/templates/template-utils.ts
+++ src/templates/template-utils.ts
@@ -4,44 +4,6 @@ import { getAllDirectoriesAndUp, getNpmPaths } from '@tps/utilities/helpers';
 import path from 'path';
 
 /**
- * All tpsrc config file names.
- *
- * @example
- *
- *	[
- *		'.tps/tps.config.cjs',
- *		'.tps/tps.config.ts',
- *		'.tps/tps.config.js',
- *		'.tps/.tpsrc.cjs',
- *		'.tps/.tpsrc.ts',
- *		'.tps/.tpsrc.js',
- *		'.tps/.tpsrc.yml',
- *		'.tps/.tpsrc.yaml',x
- *		'.tps/.tpsrc.json',
- *		'.tps/.tpsrc',
- *		'tps.config.cjs',
- *		'tps.config.ts',
- *		'tps.config.js',
- *		'.config/tpsrc.cjs',
- *		'.config/tpsrc.ts',
- *		'.config/tpsrc.js',
- *		'.config/tpsrc.yml',
- *		'.config/tpsrc.yaml',
- *		'.config/tpsrc.json',
- *		'.config/tpsrc',
- *		'.tpsrc.cjs',
- *		'.tpsrc.ts',
- *		'.tpsrc.js',
- *		'.tpsrc.yml',
- *		'.tpsrc.yaml',
- *		'.tpsrc.json',
- *		'.tpsrc',
- *		'package.json'
- *	]
- */
-// const tpsrcConfigNames: string[] = []; //tpsrcSearchPlaces;
-
-/**
  * Get all locations a template can be
  *
  * Templates can be in be:
@@ -77,32 +39,8 @@ export const findTemplate = (
 	return path.join(homeDirectory, templateName);
 };
 
-// /**
-//  * Gets path to the global .tps/ directory
-//  */
-// const getGloablTpsPath = (): string | null => {
-// 	return path.join(TPS.USER_HOME, TPS.TPS_FOLDER);
-// };
-
-// const getLocalTpsPath = (): string | null => {
-// 	const tpsLocal: string = findUp(TPS.TPS_FOLDER, TPS.CWD);
-// 	const hasLocalTpsFolder = tpsLocal && tpsLocal !== TPS.GLOBAL_PATH;
-
-// 	if (!hasLocalTpsFolder) return null;
-
-// 	return tpsLocal;
-// };
-
 export const directoryIsTpsInitialized = (
 	location: string,
 ): Promise<boolean> => {
 	return isDirAsync(path.join(location, TPS.TPS_FOLDER));
 };
-
-// const hasGloablTps = (): boolean => {
-// 	return Templates.directoryIsTpsInitialized(TPS.USER_HOME);
-// };
-
-// const hasLocalTps = (): boolean => {
-// 	return !!Templates.getLocalTpsPath();
-// };
